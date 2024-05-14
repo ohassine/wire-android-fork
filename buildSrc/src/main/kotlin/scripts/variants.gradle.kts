69b8afac65a0ada44e341d733e6246d882967780
Oussama Hassine <oussama.has100@gmail.com>
2024-05-14 14:06:44 +0200

    CI: rebuild

--- buildSrc/src/main/kotlin/scripts/variants.gradle.kts
+++ buildSrc/src/main/kotlin/scripts/variants.gradle.kts
@@ -81,10 +81,14 @@ android {
                 if (allFilesFromDir != null) {
                     val keystoreFile = allFilesFromDir.first()
                     println("keystore file name: ${keystoreFile.name}")
+                    println("keystore file path: ${keystoreFile.path}")
+                    println("keystore file absolutePath: ${keystoreFile.absolutePath}")
+                    println("keystore file canonicalPath: ${keystoreFile.canonicalPath}")
+                    println("keystore file totalSpace: ${keystoreFile.totalSpace}")
+
                     val newFilePath = Paths.get("keystore/wiretest-gradle.jks")
                     val newFile = newFilePath.toFile()
                     keystoreFile?.renameTo(newFile)
-                    println("keystore file name 2: ${keystoreFile.name}")
                 }
                 println("storePassword: ${System.getenv("KEYSTOREPWD_RELEASE")}")
                 println("keyAlias: ${System.getenv("KEYSTORE_KEY_NAME_RELEASE")}")
